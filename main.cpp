22eb1e8f91d5f25dc371883ad019db2554bd613a
CringeAcademy <157508193+CringeAcademy@users.noreply.github.com>
2025-09-26 21:53:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(){
  const char* secret  = std::getenv("PASSWORD");
   std::string s(secret);
-  if (s = "qwerty"){
+  if (s == "qwerty"){
     std::cout << "Parol +!!!!" << std::endl;
   } else {
     std::cout << "Parol -!!!!" << std::endl;
