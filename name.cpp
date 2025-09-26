6c2b261304a4f4206be26fe150a74b117909186f
CringeAcademy <157508193+CringeAcademy@users.noreply.github.com>
2025-09-26 21:50:43 +0300

    Update name.cpp

--- name.cpp
+++ name.cpp
@@ -1,12 +1,14 @@
-#include <iostring>
+#include <iostream>
 #include <cstdlib>
+#include <string>
+
 
 int main(){
  const char* secret  = std::getenv("PASSWORD");
   std::string s(secret);
   if (s = "qwerty"){
     std::cout << "Parol +!!!!" << std::endl;
-  } eles {
+  } else {
     std::cout << "Parol -!!!!" << std::endl;
 }
 return 0;
